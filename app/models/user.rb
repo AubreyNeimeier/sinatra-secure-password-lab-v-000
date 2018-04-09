bb6dea17d76249fb2972a207e0e27c9a1f022d22
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-09 20:41:36 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,9 @@ class User < ActiveRecord::Base
   #   #"Hotline Bling" => hotline-bling
   #   name.downcase.split(/\s*\W/).join("-")
   # end
-  # 
+  #
   # def self.find_by_slug(slug)
   #   #binding.pry
   #   Song.all.find {|song| song.slug == slug}
   # end
 end
-
-
