3d9076c0281ccc5e44a3e1711f8732c505b71af9
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-09 20:41:31 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  # def slug
+  #   #"Hotline Bling" => hotline-bling
+  #   name.downcase.split(/\s*\W/).join("-")
+  # end
+  # 
+  # def self.find_by_slug(slug)
+  #   #binding.pry
+  #   Song.all.find {|song| song.slug == slug}
+  # end
 end
+
+
